e93248dcb29b67242d7cb6b1b21146d9e2c79b0d
artoriaschan <dalecracker@gmail.com>
2026-04-04 12:28:47 +0800

    feat(02-03): add shared module barrel export
    - Export getOption and renderWidgetWithLabel functions
    - Re-export NaVisibility type from types.js

---
+++ packages/widget-renderer/src/shared/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Shared module exports
+ *
+ * Barrel export for shared widget utilities.
+ */
+
+export { getOption, renderWidgetWithLabel } from './widget.helper.js';
+export type { NaVisibility } from '../types.js';
